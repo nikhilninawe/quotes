6697bae9c4e3987971984ee2075f3027d15f53a9
Nikhil Ninawe <nikhil.n@turvo.com>
2017-12-17 19:29:27 +0530

    Testing share beta

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
-import { Header, ShareComponent } from './common/index';
+import { Header } from './common/index';
 import GoogleAd from './GoogleAd';
 import SnapCarousel from './SnapCarousel';
 
@@ -11,7 +11,6 @@ class Main extends Component {
             <View style={{ flex: 1, paddingTop: 20, justifyContent: 'space-between' }} >
                 <Header headerText="Inspirational Quotes" />
                 <SnapCarousel />         
-                <ShareComponent {...this.props} />
                 <GoogleAd />
             </View>
         );
